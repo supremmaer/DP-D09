448f1baf90abe1e7e5715fdf6af03fec931c54d0
Guillermo Bandrés Catalina <gbandres98@gmail.com>
2018-03-23 18:12:31 +0100

    Removed age checks from rendevous lists

--- Item 3/AcmeRendezvous/src/main/java/services/RendezvousService.java
+++ Item 3/AcmeRendezvous/src/main/java/services/RendezvousService.java
@@ -81,18 +81,9 @@ public class RendezvousService {
 
 	public Collection<Rendezvous> findAll() {
 		Collection<Rendezvous> result;
-		User user;
 
 		result = this.rendezvousRepository.findAll();
 		Assert.notNull(result);
-		user = this.userService.findByPrincipal();
-		if (user != null && !user.isAdult()) {
-			final Collection<Rendezvous> aux = new HashSet<>();
-			for (final Rendezvous r : result)
-				if (r.isAdultOnly())
-					aux.add(r);
-			result.removeAll(aux);
-		}
 
 		return result;
 	}
@@ -318,18 +309,9 @@ public class RendezvousService {
 
 	public Collection<Rendezvous> findAllFinal() {
 		Collection<Rendezvous> result;
-		User user;
 
 		result = this.rendezvousRepository.findAllFinal();
 		Assert.notNull(result);
-		user = this.userService.findByPrincipal();
-		if (user != null && !user.isAdult()) {
-			final Collection<Rendezvous> aux = new HashSet<>();
-			for (final Rendezvous r : result)
-				if (r.isAdultOnly())
-					aux.add(r);
-			result.removeAll(aux);
-		}
 
 		return result;
 	}
@@ -344,35 +326,17 @@ public class RendezvousService {
 	}
 	public Collection<Rendezvous> findByCategoryId(final int categoryId) {
 		Collection<Rendezvous> result;
-		User user;
 
 		result = this.rendezvousRepository.findByCategoryId(categoryId);
 		Assert.notNull(result);
-		user = this.userService.findByPrincipal();
-		if (user != null && !user.isAdult()) {
-			final Collection<Rendezvous> aux = new HashSet<>();
-			for (final Rendezvous r : result)
-				if (r.isAdultOnly())
-					aux.add(r);
-			result.removeAll(aux);
-		}
 
 		return result;
 	}
 	public Collection<Rendezvous> findByCategoryName(final String categoryName) {
 		Collection<Rendezvous> result;
-		User user;
 
 		result = this.rendezvousRepository.findByCategoryName(categoryName);
 		Assert.notNull(result);
-		user = this.userService.findByPrincipal();
-		if (user != null && !user.isAdult()) {
-			final Collection<Rendezvous> aux = new HashSet<>();
-			for (final Rendezvous r : result)
-				if (r.isAdultOnly())
-					aux.add(r);
-			result.removeAll(aux);
-		}
 
 		return result;
 	}
